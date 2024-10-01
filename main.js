5f8f230647fbbdc65ab16165a3364935b71b5c14
Amanda <141869873+LaraAmanda@users.noreply.github.com>
2024-10-01 11:48:08 -0300

    dc

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
-function soma(num1,num2){
-    return num1+num2;
-}
-console.log(soma(2,3));
-function multiplication(num3,num3){
-    return num3*num3;
-}
-console.log(multiplication(3,3));
-function areaquadrado(lado){
-    return lado**2;
-}
-console.log(areaquadrado(3));
-let areatrapezio = function(base1,base2,altura){
-    return base1+base2*altura/2;
-}
-console.log(areatrapezio(2,3,2));
+// function soma(num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(2,3));
+// function multiplication(num3,num3){
+//     return num3*num3;
+// }
+// console.log(multiplication(3,3));
+// function areaquadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaquadrado(3));
+// let areatrapezio = function(base1,base2,altura){
+//     return base1+base2*altura/2;
+// }
+// console.log(areatrapezio(2,3,2));
 
-let media = function(num1,num2,num3){
-return (num1*num2*num3)/3;
-}
-console.log(media(2,3,5));
+// let media = function(num1,num2,num3){
+// return (num1*num2*num3)/3;
+// }
+// console.log(media(2,3,5));
+
+let nome=['aaron','adam','amanda','ana luiza','andrew','arthur garcia','arthur henrique','caio henry','caio silva','danilo','felipe nascimento','felipe lin','felipe mayer', 'gabriela do prado','gabriela nunes','giovana','giuliana','izabela','joão henrique','joão pedro', 'julia rosa']
+let i=0;
+    while(i<10){
+    console.log(i);
+    i=i+1;
+    }
